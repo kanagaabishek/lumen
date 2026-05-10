26c96855b441578a34e5eb159e0147df1c1fdc42
kanagaabishek <abishek2981@gmail.com>
2026-05-11 04:53:07 +0530

    feat: enable tests based on Cassandra system property

--- src/test/java/com/lumen/server/unit/LumenApplicationTests.java
+++ src/test/java/com/lumen/server/unit/LumenApplicationTests.java
@@ -1,11 +1,14 @@
 package com.lumen.server.unit;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.EnabledIfSystemProperties;
+import org.junit.jupiter.api.condition.EnabledIfSystemProperty;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 
 @SpringBootTest
 @Import(TestConfiguration.class)
+@EnabledIfSystemProperty(named = "cassandra.enabled", matches = "true")
 class LumenApplicationTests {
 
 	@Test
