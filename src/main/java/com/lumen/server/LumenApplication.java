7fe6779e03f56783ad80432d397c20626de28692
kanagaabishek <abishek2981@gmail.com>
2026-05-13 07:59:16 +0530

    style: Update index.html with layout adjustments and new color theme

--- src/main/java/com/lumen/server/LumenApplication.java
+++ src/main/java/com/lumen/server/LumenApplication.java
@@ -8,6 +8,7 @@ public class LumenApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LumenApplication.class, args);
+		System.out.println("The Lumen server has started successfully. Visit http://localhost:8080 to access the Trace Explorer dashboard.");
 	}
 
 }
